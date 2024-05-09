7813ff80f3a24dea80b079801c3578053bdbb4cb
en-shungo-seino <shungo_seino@en-japan.com>
2024-05-09 13:04:44 +0900

    feat: フェッチ挙動をチェックするためにモックページ作成

---
+++ src/app/products/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+};
+
+const Products = async () => {
+  const url = `${process.env.TEST_API}/products`;
+  const products = await fetch(url).then((res) => res.json());
+  console.log(products);
+
+  return (
+    <>
+      <div>
+        {products.map((product: Product) => (
+          <ProductView key={product.id} {...product} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const ProductView = (product: Product) => (
+  <Card key={product.id}>
+    <CardHeader>
+      <CardTitle>{product.name}</CardTitle>
+      <CardDescription>{product.description}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>{product.price}</p>
+    </CardContent>
+  </Card>
+);
+
+export default Products;
